55dc9d537a337780bfe309ce03f4fbe6f3fc15e6
da <18252343715@163.com>
2025-03-31 16:42:14 +0800

    update

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -37,6 +36,13 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
+		match fruit {
+			Fruit::Apple => basket.insert(fruit, 4),
+			Fruit::Mango => basket.insert(fruit, 2),
+			Fruit::Lychee => basket.insert(fruit, 5),
+			Fruit::Banana =>basket.insert(fruit, 10),
+			Fruit::Pineapple => basket.insert(fruit, 11),
+	};
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -32,12 +31,17 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
-        }
+
+		let mut output = Vec::new();
+		for i in input {
+			match i.1 {
+				Command::Uppercase => output.push(i.0.to_uppercase()),
+				Command::Append(x) =>  output.push(i.0 + &"bar".repeat(x)),
+				Command::Trim => output.push(i.0.trim().to_string()),
+			}
+		}
         output
     }
 }
@@ -45,7 +49,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
